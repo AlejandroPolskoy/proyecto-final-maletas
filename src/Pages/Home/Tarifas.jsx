e6f78a7eeb11b3c4c8ee5e285bb29d7eafdc9f09
pacomartinz <famarcar@gmail.com>
2023-05-05 17:15:13 +0200

    tarifas first commit

--- src/Pages/Home/Tarifas.jsx
+++ src/Pages/Home/Tarifas.jsx
@@ -1,3 +1,71 @@
+import React from "react";
+import { useState } from "react";
+import "./Tarifas.scss"
+import backIcon from '../../assets/icons8Back100Copy@3x.png';
+
+
+
 export default function Tarifas() {
-    return "Esto es tarifas";
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    
+    }
+
+    return(
+
+        <div className="container">
+            
+            <a href="javascript:history.back()">
+                <img className="atras" src={backIcon} alt="back"></img>
+            </a>
+            
+
+            <div className="selecciona__container">
+                <div>
+                    <p className="subtitulo">Selecciona</p>
+                </div>
+                <select className="select__container--caja" id="options" value={selectedOption} onChange={handleOptionChange}>
+                    <option className="select__item" value="opcion1">Europa</option>
+                    <option className="select__item" value="opcion2">Asia</option>
+                    <option className="select__item" value="opcion3">América</option>
+                    <option className="select__item" value="opcion4">África</option>
+                    <option className="select__item" value="opcion5">Oceanía</option>
+
+                </select>
+            </div>
+
+
+            <div className="tarifas__container">
+
+               
+                <p className="subtitulo">Nuestras tarifas fijas</p>
+                
+
+                <div className="tarifas__tarjetas">
+
+                    <div className="tarifas__tarjeta01">
+                        <p className="tarifas__tarjeta--texto">24 Horas</p>
+                        <p className="tarifas__tarjeta--numero">6€</p>
+                        <p className="tarifas__tarjeta--texto">Por equipaje</p>
+                    </div>
+
+                    <div className="tarifas__tarjeta02">
+                        <p className="tarifas__tarjeta--texto">Día adicional</p>
+                        <p className="tarifas__tarjeta--numero">4€</p>
+                        <p className="tarifas__tarjeta--texto">Por equipaje</p>
+                    </div>
+                </div>
+            
+            
+
+            </div>
+
+        </div>
+
+
+
+    )
 }
