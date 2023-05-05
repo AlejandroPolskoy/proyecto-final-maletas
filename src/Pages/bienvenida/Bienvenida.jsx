f0292078c9f35e67ae2031d3d355d1c58f364eb6
Cristelos <cristelos@msn.com>
2023-05-05 15:45:08 +0200

    Pantalla bienvenida terminada

--- src/Pages/bienvenida/Bienvenida.jsx
+++ src/Pages/bienvenida/Bienvenida.jsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from 'react';
+import { Modal } from '../../Componentes/Modal/Modal';
+import './Bienvenida.scss'
+
 export default function Bienvenida() {
-    return "Esto es bienvenida";
+    const [modal, setModal] = useState(false);
+    const Toggle = () => setModal(!modal);
+
+    
+    useEffect(() => {
+        setTimeout(Toggle, 2500)}, 
+        []);
+
+    return (
+        <div className='bienvenida'>
+            <img src='assets/logo-maleteo.png' alt='logo maleteo' className='bienvenida_img'/>
+            <Modal mostrar={modal} close={Toggle}/> 
+        </div>
+    );
 }
