19cac6023f4df595085d2ddce0db12f9bd8db8a2
Oriiromero <oriana.nahir.r@gmail.com>
2023-05-06 11:34:22 +0200

    Novedades done

--- src/Componentes/Novedades/Novedades.jsx
+++ src/Componentes/Novedades/Novedades.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './Novedades.scss';
+import {motion} from 'framer-motion';
 
 const Novedades = () => {
+
+  const images = [
+    {
+      id: 1,
+      src: '/assets/img1.jpeg',
+      alt: 'img1'
+    }, {
+      id: 1,
+      src: '/assets/img2.jpeg',
+      alt: 'img2'
+    },{
+      id: 1,
+      src: '/assets/img3.jpeg',
+      alt: 'img3'
+    }
+  ]
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='news-container'>
+        <h1 className='news-container-title'>Novedades</h1>
+        <motion.div className='slider-container'>
+          <motion.div className='slider' drag='x' dragConstraints={{right: 2, left: -504}}>
+            {images.map(image =>{ return (
+                <motion.div className='item-cont item' style={{'background-image': `url(${image.src})`}}>
+                    <h4 className='item-cont_title'> Blog </h4>
+                    <p className='item-cont_p'> Un nuevo articulo de nuestro viajero empedernido.</p>
+                    <p className='item-cont_a'> Leer mas </p>
+              </motion.div>
+            )})}
+          </motion.div>
+        </motion.div>
+      </div>
+     
+    </>
+  
   )
 }
 
