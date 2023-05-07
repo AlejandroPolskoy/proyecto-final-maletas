15736aaf34ac261703510f81da19d6ed75f6140e
Oriiromero <oriana.nahir.r@gmail.com>
2023-05-07 18:27:43 +0200

    modal schema done (not functional)

--- src/Componentes/Busqueda/Busqueda.jsx
+++ src/Componentes/Busqueda/Busqueda.jsx
@@ -10,6 +10,24 @@ const Busqueda = ({closeModal, setAddress}) => {
             <img className='aspa' src='/assets/aspa@2x.png' alt='aspa' />
             <input className="input" type="text" placeholder="¿Dónde te encuentras? Madrid, Barcelona…"/>
         </div>
+
+        <div className='history' >
+            <div className='history-one'>
+                <img src='/assets/brujula@2x.png' alt='brujula' />
+                <p> Utilizar mi ubicación actual </p>
+            </div>
+            <div className='history-two'>
+                <img src='/assets/reloj@2x.png' alt='reloj' />
+                <p> Pacífico, Madrid </p>
+            </div>
+            <div className='history-two'>
+                <img src='/assets/reloj@2x.png' alt='reloj' />
+                <p> Alicante, España</p>
+            </div>
+        </div>
+        <div className='next'>
+            <img src='/assets/botonContinuar@2x.png' alt='next' />
+        </div>
     </div>
   )
 }
