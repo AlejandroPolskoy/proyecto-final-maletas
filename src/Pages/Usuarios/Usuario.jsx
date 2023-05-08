b99ee62062dca20d6b79d3860a1abb551cb050b8
Cristelos <cristelos@msn.com>
2023-05-08 15:36:01 +0200

    Lista la maquetación

--- src/Pages/Usuarios/Usuario.jsx
+++ src/Pages/Usuarios/Usuario.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from "../../Componentes/Footer/Footer";
+import { Link } from "react-router-dom";
 import './Usuario.scss'
 
 export default function Usuario() {
@@ -12,13 +13,39 @@ export default function Usuario() {
         <div key={index} className="user_head">
             <div className="user_detail">
                 <h2 className="user_title">{list.name}</h2>
-                <p className="user_subtitle">Puedes ver y editar tu perfil</p>
+                <p className="user_subtitle"><Link to="/">Puedes ver y editar tu perfil</Link></p>
             </div>
             <div className="user_detail">
                 <img src={list.img} alt={list.name} className="user_img"/>
             </div>
         </div>
         )}
+        <div className="user_list">
+            <div className="user_list_details">
+                <h4>Conviértete en guardián</h4>
+                <p>Puedes ganar 400€ de media al mes</p>
+            </div>
+            <div className="user_list_details">
+                <h4>Invita a tus amigos</h4>
+                <p>Y podrás ganar descuentos para ti</p>
+            </div>
+            <div className="user_list_details">
+                <h4>Créditos y descuentos</h4>
+                <p></p>
+            </div>
+            <div className="user_list_details">
+                <h4>Publica tu anuncio o experiencia</h4>
+                <p></p>
+            </div>
+            <div className="user_list_details">
+                <h4>Configuración</h4>
+                <p></p>
+            </div>
+            <div className="user_list_details">
+                <h4>Ayuda</h4>
+                <p></p>
+            </div>
+        </div>
         <Footer/>
     </div>);
 }
