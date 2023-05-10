dd90923f5e1a81031f319b2a26ea60fb57efe828
Cristelos <cristelos@msn.com>
2023-05-10 10:19:40 +0200

    Con problemas con las estrellas

--- src/Componentes/shared.jsx
+++ src/Componentes/shared.jsx
@@ -1,2 +1,2 @@
-const urls = ["https://maleteo-node.vercel.app", "http://localhost:8888"];
-export const api = urls[1];
+const urls = ["https://maleteo-node.vercel.app", "http://localhost:8888","http://188.76.175.40:37404"];
+export const api = urls[2];
--- src/Pages/Anuncios/ListaAnuncios.jsx
+++ src/Pages/Anuncios/ListaAnuncios.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { api } from "../../Componentes/shared";
 import { VariablesContext } from "../../Shared/VariablesContext";
 import axios from "axios";
+import './ListaAnuncios.scss'
 
 export default function ListaAnuncios() {
     const navigate = useNavigate();
@@ -25,21 +26,45 @@ export default function ListaAnuncios() {
         })
     }
 
-    function selecteChanged(e) {
-        console.log( e.target.value );
-        setReserva({...reserva, anuncio: e.target.value})
+    function selecteChanged(e, id) {
+        console.log( id);
+        setReserva({...reserva, anuncio: id})
     }
 
     function reservar() {
         navigate("/detallesreserva");
     }
 
-    return <div>
-        <select onChange={selecteChanged}>
+    let pixelPercentage = listaAnuncios.stars;
+    pixelPercentage = 100 / 5 * listaAnuncios.stars;
+
+    return (
+        <div className="lista-anuncios">
         { listaAnuncios && listaAnuncios.map((anuncio, index) => 
-            <option key="index" value={anuncio._id}>{anuncio.title}</option>
+        <div onClick={(e) => selecteChanged(e, anuncio._id)}  key={index} >
+            <div className="lista-anuncios_contenedor">
+                <div className="lista-anuncios_details">
+                    <img src={anuncio.image} alt={anuncio.title} className="lista-anuncios_img"/>
+                </div>
+                <div className="lista-anuncios_details">
+                    <h3>{anuncio.title}</h3>
+                    <h4>{anuncio.owner && anuncio.owner.name}</h4>
+                    <img src={anuncio.owner && anuncio.owner.image} alt={anuncio.owner && anuncio.owner.name} className="lista-anuncios_profile"/>
+                    <div className='star-file'>
+                    <div className='star-file-inner' style={{'width': pixelPercentage}}>
+                        <img className='star-file-star' src='/assets/Star_1.png' alt='star' />
+                        <img className='star-file-star' src='/assets/Star_1.png' alt='star' />
+                        <img className='star-file-star' src='/assets/Star_1.png' alt='star' />
+                        <img className='star-file-star' src='/assets/Star_1.png' alt='star' />
+                        <img className='star-file-star' src='/assets/Star_1.png' alt='star' />
+                    </div>
+                <p className='random'> {`(${comments.length})`} </p>
+            </div>
+                </div>
+            </div>
+        </div>
         )}
-        </select>
         <div><button onClick={reservar}>RESERVAR</button></div>
-    </div>
+        </div>
+    )
 }
