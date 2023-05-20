05ecd4f8983d1efaea084c67285bfe61fa7360c3
AlejandroPolskoy <mitchel_nk@yahoo.es>
2023-05-20 15:28:16 +0200

    vercel test

--- src/Componentes/shared.jsx
+++ src/Componentes/shared.jsx
@@ -1,5 +1,5 @@
 const urls = ["https://maleteo-node.vercel.app", "http://localhost:8888", "http://188.76.175.40:37404"];
-export const api = urls[1];
+export const api = urls[0];
 
 export const formatDate = (date) => {
     return (typeof date) === "object" ? date.toLocaleDateString('en-GB') : new Date(date).toLocaleDateString('en-GB');
